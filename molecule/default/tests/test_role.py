08275723a8ce88d5374a23e64ea207ea472bc1ff
Nicolas Boutet <amd3002@gmail.com>
2023-04-26 13:26:09 +0200

    Adapt CI workflow for molecule 5 (#7)
    * Adapt CI workflow for molecule 5
    
    * Add .ansible-lint
    
    * Fix yamllint violations
    
    * Fix depreciation warning

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,22 +5,29 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('awscli'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("awscli"),
+    ],
+)
 def test_awscli_is_installed(host, name):
-    packages = host.pip_package.get_packages(pip_path='pip3')
+    packages = host.pip.get_packages(pip_path="pip3")
     assert name in packages
 
 
-@pytest.mark.parametrize('username,groupname,s3_config', [
-  ('test_user', 'test_group', 's3'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,s3_config",
+    [
+        ("test_user", "test_group", "s3"),
+    ],
+)
 def test_awscli_config_file(host, username, groupname, s3_config):
-    awscli_config = host.file('/home/' + username + '/.aws/config')
+    awscli_config = host.file("/home/" + username + "/.aws/config")
     assert awscli_config.exists
     assert awscli_config.is_file
     assert awscli_config.user == username
@@ -28,11 +35,14 @@ def test_awscli_config_file(host, username, groupname, s3_config):
     assert awscli_config.contains(s3_config)
 
 
-@pytest.mark.parametrize('username,groupname,key,secret', [
-  ('test_user', 'test_group', 'aaaa', 'bbbb'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,key,secret",
+    [
+        ("test_user", "test_group", "aaaa", "bbbb"),
+    ],
+)
 def test_awscli_credentials_file(host, username, groupname, key, secret):
-    awscli_credentials = host.file('/home/' + username + '/.aws/credentials')
+    awscli_credentials = host.file("/home/" + username + "/.aws/credentials")
     assert awscli_credentials.exists
     assert awscli_credentials.is_file
     assert awscli_credentials.user == username
